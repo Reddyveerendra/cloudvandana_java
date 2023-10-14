abaee61f31fa91b32bd894cdf8ddc4b76cee5518
Reddyveerendra <reddyveerendra535@gmail.com>
2023-10-14 18:29:17 +0530

    roman num to int

--- RomanToInt.java
+++ RomanToInt.java
@@ -1,10 +1,52 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class RomanToInt {
+    public int romanToInt(String s) {
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char currentSymbol = s.charAt(i);
+            int currentValue = getValue(currentSymbol);
+
+            if (currentValue < prevValue) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+
+            prevValue = currentValue;
+        }
+
+        return result;
+    }
+
+    public int getValue(char symbol) {
+        switch (symbol) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                throw new IllegalArgumentException("Invalid Roman numeral: " + symbol);
+        }
+    }
+
     public static void main(String[] args) {
+        RomanToInt converter = new RomanToInt();
         Scanner scan = new Scanner(System.in);
-        String str = scan.nextLine();
-        
-        System.out.println(str);
+        String romanNumeral = scan.nextLine();
+        int result = converter.romanToInt(romanNumeral);
+        System.out.println("Roman numeral " + romanNumeral + " is equal to " + result);
     }
 }
